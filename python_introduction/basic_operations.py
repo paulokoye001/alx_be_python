c7733a867ef4ae7edec8bc8bd6413cbbbf6ed4ff
PAUL OKOYE <paulokoye125@gmail.com>
2025-01-08 06:48:23 -0800

    add solution to task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+addition = 10 + 5
+
+Subtraction = 10 - 5
+
+Multiplication = 10 * 5
+
+print(f"Addition of 10 and 5 is {addition}")
+
+print(f"Subtraction of 10 and 5 is {Subtraction}")
+
+print(f"Multiplication  of 10 and 5 is {Multiplication}")
+
+
