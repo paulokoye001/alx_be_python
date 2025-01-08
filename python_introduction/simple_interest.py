21eb3b95ec5a3b0c5b4e9663ef397b57034bb0d2
PAUL OKOYE <paulokoye125@gmail.com>
2025-01-08 07:49:01 -0800

    2nd commit task

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,10 +3,10 @@
 #   R = the annual interest rate (as a decimal),
 #   T = the time the money is invested for in years.
 
-P = 1000 # in dollars ($)
-R = 0.05 # 5%
-T = 3 # 3 years
+principal = 1000 # in dollars ($)
+rate = 0.05 # 5%
+time = 3 # 3 years
 
-interest = P * R * T
+interest = principal * rate *time
 
 print ('the simple interest ' + str(interest))
