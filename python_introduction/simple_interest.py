5648000f471fdbf5d35f36ed348dec2b98d6d74f
PAUL OKOYE <paulokoye125@gmail.com>
2025-01-08 07:44:05 -0800

    task 1 lines of code

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+#   I = the interest earned,
+#   P = the principal amount (initial investment),
+#   R = the annual interest rate (as a decimal),
+#   T = the time the money is invested for in years.
+
+P = 1000 # in dollars ($)
+R = 0.05 # 5%
+T = 3 # 3 years
+
+interest = P * R * T
+
+print ('the simple interest ' + str(interest))
