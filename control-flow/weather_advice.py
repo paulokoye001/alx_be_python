092303f1b4e8fd0902b148d2a585233706917968
PAUL OKOYE <paulokoye125@gmail.com>
2025-01-20 08:49:22 -0800

    3rd change

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-weather = str (input("What's the weather like today? (sunny/rainy/cold):")).lower()
+weather = (input("What's the weather like today? (sunny/rainy/cold):")).lower()
 
 
 if weather == "sunny":
