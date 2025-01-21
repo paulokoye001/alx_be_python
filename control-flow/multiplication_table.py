ecb58f1e629da9107d12c5ad77259a52bb3586c5
PAUL OKOYE <paulokoye125@gmail.com>
2025-01-21 04:40:08 -0800

    removed the end command

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,4 +4,4 @@ number = int (input ("Enter a number to see its multiplication table: "))
 for j in range(1, 11):
     # Inner loop iterates through columns (other factors)
     product = number * j
-    print(f"{number} x {j} = {product}", end="\n")  # Print with tabs for better formatting
+    print(f"{number} x {j} = {product}")  # Print with tabs for better formatting
