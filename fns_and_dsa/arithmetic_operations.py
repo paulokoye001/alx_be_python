c22b00840919c5d9d36fe6b392f0e139b913e2dd
PAUL OKOYE <paulokoye125@gmail.com>
2025-02-01 08:01:25 -0800

    second commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,7 @@
 
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """Performs basic arithmetic operations based on the given operation string."""
     if operation == 'add':
         return num1 + num2
