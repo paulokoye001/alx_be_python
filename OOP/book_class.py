02cca0ba570594a9efb3a3b546bb6a1f09552615
PAUL OKOYE <paulokoye125@gmail.com>
2025-03-17 09:57:18 -0700

    Initial commit

---
+++ OOP/book_class.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title, author, year):
+        """Constructor to initialize book attributes"""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor to print a message when an instance is deleted"""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """String representation for informal display"""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Official representation for recreating the instance"""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
